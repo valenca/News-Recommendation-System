698f21c20a4b91a55dc1bdc6651523ae32cd9e52
Gustavo Martins <gus.a.martins@gmail.com>
2014-04-17 17:57:05 +0100

    extract

---
+++ Projecto/extract.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import urllib
+
+url = "http://www.bbc.co.uk/news/world-europe-27065782#sa-ns_mchannel=rss&amp;ns_source=PublicRSS20-sa"
+html = urllib.urlopen(url).read()
+
+soup = BeautifulSoup(html,'html5lib')
+print str(soup.title)[7:-8]
+content = [div for div in soup.find_all('div') if str(div)[:24] == '<div class="story-body">']
+soup = BeautifulSoup(str(content[0]),'html5lib')
+[par.extract() for par in soup.findAll('p') if str(par.findParent())[:24] != '<div class="story-body">']
+paragraphs = [par for par in soup.findAll('p')]
+soup = BeautifulSoup(' '.join(list(map(str,paragraphs))),'html5lib')
+print(soup.get_text())
+
