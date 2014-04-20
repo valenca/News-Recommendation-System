c0c007ed00724df7a6e0122fa33cffa2f5f28d86
Gustavo Martins <gus.a.martins@gmail.com>
2014-04-20 11:14:52 +0100

    retrieval

--- Projecto/Database/create_tables.sql
+++ Projecto/Database/create_tables.sql
@@ -1,62 +1,75 @@
 CREATE TABLE users (
-	u_id INTEGER NOT NULL,
-	u_name TEXT NOT NULL,
-	u_username TEXT NOT NULL,
-	u_password TEXT NOT NULL,
-	PRIMARY KEY (u_id)
+	usr_id INTEGER NOT NULL,
+	usr_name TEXT NOT NULL,
+	usr_username TEXT NOT NULL,
+	usr_password TEXT NOT NULL,
+	usr_nviews INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY (usr_id)
 );
 
-CREATE TABLE news (
-	n_id INTEGER NOT NULL,
-	n_datetime DATETIME NOT NULL,
-	n_link TEXT NOT NULL,
-	n_thumbnail TEXT NOT NULL,
-	n_title TEXT NOT NULL,
-	n_description TEXT NOT NULL,
-	n_text TEXT NOT NULL,
-	n_topic INTEGER NOT NULL REFERENCES topics(t_id),
-	n_rating REAL NOT NULL DEFAULT 0,
-	n_total_ratings INTEGER NOT NULL DEFAULT 0,
-	n_total_views INTEGER NOT NULL DEFAULT 0,
-	PRIMARY KEY (n_id)
+CREATE TABLE documents (
+	doc_id INTEGER NOT NULL,
+	doc_datetime DATETIME NOT NULL,
+	doc_link TEXT NOT NULL,
+	doc_thumbnail TEXT NOT NULL,
+	doc_title TEXT NOT NULL,
+	doc_description TEXT NOT NULL,
+	doc_text TEXT NOT NULL,
+	doc_topic INTEGER NOT NULL REFERENCES topics(tpc_id),
+	doc_rating REAL NOT NULL DEFAULT 2.5,
+	doc_nratings INTEGER NOT NULL DEFAULT 0,
+	doc_nviews INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY (doc_id)
 );
 
 CREATE TABLE topics (
-	t_id INTEGER NOT NULL,
-	t_name TEXT NOT NULL,
-	PRIMARY KEY (t_id)
-);
-
-CREATE TABLE evaluations (
-	e_user INTEGER NOT NULL REFERENCES users(u_id),
-	e_news INTEGER NOT NULL REFERENCES news(n_id),
-	e_rating INTEGER NOT NULL,
-	PRIMARY KEY (e_user, e_news)
-);
-
-CREATE TABLE views (
-	v_user INTEGER NOT NULL REFERENCES users(u_id),
-	v_news INTEGER NOT NULL REFERENCES news(n_id),
-	PRIMARY KEY (v_user, v_news)
-);
-
-CREATE TABLE recommendations (
-	r_user INTEGER NOT NULL REFERENCES users(u_id),
-	r_news INTEGER NOT NULL REFERENCES news(n_id),
-	r_rating REAL NOT NULL,
-	PRIMARY KEY (r_user, r_news)
-);
-
-CREATE TABLE preferences (
-	p_user INTEGER NOT NULL REFERENCES users(u_id),
-	p_topic INTEGER NOT NULL REFERENCES topics(t_id),
-	p_rating REAL NOT NULL,
-	PRIMARY KEY (p_user, p_topic)
+	tpc_id INTEGER NOT NULL,
+	tpc_name TEXT NOT NULL,
+	PRIMARY KEY (tpc_id)
 );
 
 CREATE TABLE feeds (
-	f_id INTEGER NOT NULL,
-	f_topic INTEGER NOT NULL REFERENCES topics(t_id),
-	f_link TEXT NOT NULL,
-	PRIMARY KEY (f_id)
+	fds_id INTEGER NOT NULL,
+	fds_topic INTEGER NOT NULL REFERENCES topics(tpc_id),
+	fds_link TEXT NOT NULL,
+	PRIMARY KEY (fds_id)
+);
+
+CREATE TABLE tags (
+	tgs_id INTEGER NOT NULL,
+	tgs_tag TEXT NOT NULL,
+	PRIMARY KEY (tgs_id)
+);
+
+CREATE TABLE marks (
+	mrk_tag INTEGER NOT NULL REFERENCES tags(tgs_id),
+	mrk_document INTEGER NOT NULL REFERENCES documents(doc_id),
+	PRIMARY KEY (mrk_tag, mrk_document)
+);
+ 
+
+CREATE TABLE historics (
+	hst_user INTEGER NOT NULL REFERENCES users(usr_id),
+	hst_document INTEGER NOT NULL REFERENCES documents(doc_id),
+	hst_rating INTEGER NOT NULL DEFAULT -1,
+	hst_view INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY (hst_user, hst_document)
+);
+
+CREATE TABLE tpc_preferences (
+	tpp_user INTEGER NOT NULL REFERENCES users(usr_id),
+	tpp_topic INTEGER NOT NULL REFERENCES topics(tpc_id),
+	tpp_nviews INTEGER NOT NULL DEFAULT 0,
+	tpp_rating REAL NOT NULL DEFAULT 2.5,
+	tpp_nratings INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY (tpp_user, tpp_topic)
+);
+
+CREATE TABLE tgs_preferences (
+	tgs_user INTEGER NOT NULL REFERENCES users(usr_id),
+	tgs_tag INTEGER NOT NULL REFERENCES tags(tgs_id),
+	tgs_nviews INTEGER NOT NULL DEFAULT 0,
+	tgs_rating REAL NOT NULL DEFAULT 2.5,
+	tgs_nratings INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY (tgs_user, tgs_tag)
 );
