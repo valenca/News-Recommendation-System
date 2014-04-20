1adbbd03ebdf761794d874427a217891d0ac6b10
jsvalenca <jsvalenca@gmail.com>
2014-04-20 20:57:55 +0100

    functionize

--- Projecto/Database/retrieval.py
+++ Projecto/Database/retrieval.py
@@ -8,68 +8,56 @@ from nltk.tokenize import sent_tokenize
 from dateutil import parser
 from termcolor import cprint
 
-class Retrieval:
+def retrieve():
 
-	def __init__(self):
-		self.database = connect('database.db')
-		self.get_database_data()
-		self.get_rss_info()
-		self.get_documents()
+	def get_database_data():
+		for row in database.execute('SELECT * FROM topics;'):
+			topics.append([row[0], str(row[1])])
+		for row in database.execute('SELECT fds_topic, fds_link FROM feeds;'):
+			feeds.append([row[0],str(row[1])])		
+		for row in database.execute('SELECT doc_id, doc_datetime, doc_link FROM documents'):
+			documents.append([row[0],str(row[1]),str(row[2])])
+		#pprint(topics)
+		#pprint(feeds)
+		#pprint(documents)
 
-		#print [int(row[0]) for row in self.database.execute('SELECT doc_id FROM documents'+\
-		#	' WHERE doc_processed = 0;')]
-
-	def get_database_data(self):
-		self.topics, self.feeds, self.documents = [[],[],[]]
-		for row in self.database.execute('SELECT * FROM topics;'):
-			self.topics.append([row[0], str(row[1])])
-		for row in self.database.execute('SELECT fds_topic, fds_link FROM feeds;'):
-			self.feeds.append([row[0],str(row[1])])		
-		for row in self.database.execute('SELECT doc_id, doc_datetime, doc_link FROM documents'):
-			self.documents.append([row[0],str(row[1]),str(row[2])])
-		#pprint(self.topics)
-		#pprint(self.feeds)
-		#pprint(self.documents)
-
-	def get_rss_info(self):
-		self.titles, self.descriptions, self.links, self.datetimes = [[],[],[],[]]
-		self.thumbnails, self.doc_topics = [[],[]]
-		for topic, link in self.feeds:
+	def get_rss_info():
+		for topic, link in feeds:
 			html = urlopen(link).read()
 			soup = BeautifulSoup(html)
 			items = [item for item in soup.find_all('item')]
 			for item in items:
-				self.doc_topics.append(topic)
+				doc_topics.append(topic)
 				if item.title is not None:
 					title = item.title.findAll(text=True)
-					if len(title) == 1: self.titles.append(title[0].encode('ascii',errors='ignore'))
-					else:               self.titles.append('')
+					if len(title) == 1: titles.append(title[0].encode('ascii',errors='ignore'))
+					else:               titles.append('')
 				if item.description is not None:
 					desc = item.description.findAll(text=True)
-					if len(desc) == 1: self.descriptions.append(desc[0].encode('ascii',errors='ignore'))
-					else:              self.descriptions.append('')
+					if len(desc) == 1: descriptions.append(desc[0].encode('ascii',errors='ignore'))
+					else:              descriptions.append('')
 				if item.guid is not None:
 					link = item.guid.findAll(text=True)
-					if len(link) == 1: self.links.append(link[0].encode('ascii',errors='ignore'))
-					else:              self.links.append('')
+					if len(link) == 1: links.append(link[0].encode('ascii',errors='ignore'))
+					else:              links.append('')
 				if item.pubdate is not None:
 					date = item.pubdate.findAll(text=True)
-					if len(date) == 1: self.datetimes.append(date[0].encode('ascii',errors='ignore'))
-					else:              self.datetimes.append('')
+					if len(date) == 1: datetimes.append(date[0].encode('ascii',errors='ignore'))
+					else:              datetimes.append('')
 				thumb = item.findChildren('media:thumbnail',{'width':'144'})
-				if len(thumb) == 1: self.thumbnails.append(thumb[0]['url'].encode('ascii',errors='ignore'))
-				else:               self.thumbnails.append('')
+				if len(thumb) == 1: thumbnails.append(thumb[0]['url'].encode('ascii',errors='ignore'))
+				else:               thumbnails.append('')
 
-	def get_documents(self):
+	def get_documents():
 		new = 0
 		updated = 0
-		for index in range(len(self.titles)):
-			print('('+str(index+1).ljust(4) + str(self.doc_topics[index]).ljust(3) + ')'),
+		for index in range(len(titles)):
+			print('('+str(index+1).ljust(4) + str(doc_topics[index]).ljust(3) + ')'),
 			
-			datetime = parser.parse(self.datetimes[index])
+			datetime = parser.parse(datetimes[index])
 			try:
-				pos = [doc[2] for doc in self.documents].index(self.links[index])
-				if str(datetime) == str(self.documents[pos][1]):
+				pos = [doc[2] for doc in documents].index(links[index])
+				if str(datetime) == str(documents[pos][1]):
 					cprint('Unchanged Article','white',end='\n\b')
 					continue
 				refresh = 1
@@ -77,11 +65,11 @@ class Retrieval:
 				refresh = 0
 
 			not_article = ('VIDEO','AUDIO','In pictures','Your pictures')
-			if self.titles[index].startswith(not_article):
+			if titles[index].startswith(not_article):
 				cprint('Not an Article','yellow',end='\n\b')
 				continue
 
-			html = urlopen(self.links[index]).read()
+			html = urlopen(links[index]).read()
 			soup = BeautifulSoup(html)
 			title = str(soup.title)[7:-8].decode('utf-8').encode('ascii',errors='ignore')
 
@@ -113,28 +101,37 @@ class Retrieval:
 			text = title + '\n' + text
 
 			if refresh == 1:
-				self.database.execute('UPDATE documents SET doc_processed = 0,'+
+				database.execute('UPDATE documents SET doc_processed = 0,'+
 					' doc_datetime = \''+str(datetime)+'\','+\
-					' doc_thumbnail = \''+self.thumbnails[index]+'\','+\
-					' doc_title = \''+self.titles[index].replace('\'','\'\'')+'\','+\
-					' doc_description = \''+self.descriptions[index].replace('\'','\'\'')+'\','+\
+					' doc_thumbnail = \''+thumbnails[index]+'\','+\
+					' doc_title = \''+titles[index].replace('\'','\'\'')+'\','+\
+					' doc_description = \''+descriptions[index].replace('\'','\'\'')+'\','+\
 					' doc_text = \''+text.replace('\'','\'\'')+'\''+\
-					' WHERE doc_link = \''+self.links[index]+'\';')
-				cprint('Update - '+self.titles[index],'cyan',attrs=['bold'],end='\n\b')
+					' WHERE doc_link = \''+links[index]+'\';')
+				cprint('Update - '+titles[index],'cyan',attrs=['bold'],end='\n\b')
 				updated += 1
 			else:
-				self.documents.append([len(self.documents), self.titles[index], datetime])
-				self.database.execute('INSERT INTO documents (doc_datetime, doc_link, doc_thumbnail,'+\
+				documents.append([len(documents), titles[index], datetime])
+				database.execute('INSERT INTO documents (doc_datetime, doc_link, doc_thumbnail,'+\
 					' doc_title, doc_description, doc_text, doc_topic) VALUES (\''+\
-					str(datetime)+'\',\''+self.links[index]+'\',\''+self.thumbnails[index]+'\',\''+\
-					self.titles[index].replace('\'','\'\'')+'\',\''+\
-					self.descriptions[index].replace('\'','\'\'')+'\',\''+\
-					text.replace('\'','\'\'')+'\','+str(self.doc_topics[index])+');')
-				cprint('Insert - '+self.titles[index],'green',attrs=['bold'],end='\n\b')
+					str(datetime)+'\',\''+links[index]+'\',\''+thumbnails[index]+'\',\''+\
+					titles[index].replace('\'','\'\'')+'\',\''+\
+					descriptions[index].replace('\'','\'\'')+'\',\''+\
+					text.replace('\'','\'\'')+'\','+str(doc_topics[index])+');')
+				cprint('Insert - '+titles[index],'green',attrs=['bold'],end='\n\b')
 				new += 1
 
-			self.database.commit()
+			database.commit()
 		print new,"new,", updated,"updated."
-if __name__ == "__main__":
+	
+	database = connect('database.db')
+	topics, feeds, documents,titles,descriptions,links,datetimes,thumbnails,doc_topics = [[],[],[],[],[],[],[],[],[]]
+	get_database_data()
+	get_rss_info()
+	get_documents()
 
-	Retrieval()
+	#print [int(row[0]) for row in database.execute('SELECT doc_id FROM documents'+\
+	#	' WHERE doc_processed = 0;')]
+
+if __name__ == "__main__":
+	retrieve()
