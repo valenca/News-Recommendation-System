bc774a76a1a203d72912303692efb3af16390259
Gustavo Martins <gus.a.martins@gmail.com>
2014-04-22 09:38:41 +0100

    indexing

--- Projecto/Database/retrieval.py
+++ Projecto/Database/retrieval.py
@@ -18,7 +18,6 @@ def retrieve():
 	for row in database.execute('SELECT doc_id, doc_datetime, doc_link FROM documents'):
 		documents.append([row[0],str(row[1]),str(row[2])])
 
-<<<<<<< HEAD
 	### GET RSS INFO
 	for topic, link in feeds:
 		html = urlopen(link).read()
@@ -45,23 +44,6 @@ def retrieve():
 			thumb = item.findChildren('media:thumbnail',{'width':'144'})
 			if len(thumb) == 1: thumbnails.append(thumb[0]['url'].encode('ascii',errors='ignore'))
 			else:               thumbnails.append('')
-=======
-	def get_documents():
-		new = 0
-		updated = 0
-		for index in range(len(titles)):
-			print('('+str(index+1).ljust(4) + str(doc_topics[index]).ljust(2) + ')'),
-			
-			datetime = parser.parse(datetimes[index])
-			try:
-				pos = [doc[2] for doc in documents].index(links[index])
-				if str(datetime) == str(documents[pos][1]):
-					print('Unchanged Article')
-					continue
-				refresh = 1
-			except:
-				refresh = 0
->>>>>>> a7cbd74a8d9f40b0ab24868de33bf699acaba188
 
 	### GET DOCUMENTS
 	new = 0
@@ -107,7 +89,6 @@ def retrieve():
 			print('Empty Text')
 			continue
 
-<<<<<<< HEAD
 		rsub(' +',' ',text)
 		text = text.strip()
 		text = '\n'.join([sentence for sentence in sent_tokenize(text)])
@@ -140,38 +121,6 @@ def retrieve():
 		database.commit()
 		
 	print new,"new,", updated,"updated."
-=======
-			if refresh == 1:
-				database.execute('UPDATE documents SET doc_processed = 0,'+
-					' doc_datetime = \''+str(datetime)+'\','+\
-					' doc_thumbnail = \''+thumbnails[index]+'\','+\
-					' doc_title = \''+titles[index].replace('\'','\'\'')+'\','+\
-					' doc_description = \''+descriptions[index].replace('\'','\'\'')+'\','+\
-					' doc_text = \''+text.replace('\'','\'\'')+'\''+\
-					' WHERE doc_link = \''+links[index]+'\';')
-				print('Update - '+titles[index])
-				updated += 1
-			else:
-				documents.append([len(documents), titles[index], datetime])
-				database.execute('INSERT INTO documents (doc_datetime, doc_link, doc_thumbnail,'+\
-					' doc_title, doc_description, doc_text, doc_topic) VALUES (\''+\
-					str(datetime)+'\',\''+links[index]+'\',\''+thumbnails[index]+'\',\''+\
-					titles[index].replace('\'','\'\'')+'\',\''+\
-					descriptions[index].replace('\'','\'\'')+'\',\''+\
-					text.replace('\'','\'\'')+'\','+str(doc_topics[index])+');')
-				print('Insert - '+titles[index])
-				new += 1
-
-			database.commit()
-		print new,"new,", updated,"updated."
-	
-	database = connect('database.db')
-	topics,feeds,documents,titles,descriptions = [[],[],[],[],[]]
-	links,datetimes,thumbnails,doc_topics = [[],[],[],[]]
-	get_database_data()
-	get_rss_info()
-	get_documents()
->>>>>>> a7cbd74a8d9f40b0ab24868de33bf699acaba188
 
 if __name__ == "__main__":
 	retrieve()
