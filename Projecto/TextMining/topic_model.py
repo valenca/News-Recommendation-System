7b7a6e67e0c83fbfebbbe5548d2549ed2cfddc9e
jsvalenca <jsvalenca@gmail.com>
2014-05-05 23:54:36 +0100

    topic modelling v2

--- Projecto/TextMining/topic_model.py
+++ Projecto/TextMining/topic_model.py
@@ -1,7 +1,7 @@
 from gensim.models.ldamodel import LdaModel
 from gensim.models import TfidfModel
 
-from gensim.corpora.dictionary import Dictionary
+from gensim.corpora import Dictionary
 
 from cPickle import load
 from pprint import pprint
@@ -12,25 +12,63 @@ class MyCorpus(object):
 		total=load(f)
 		for i in range(total):
 			yield dictionary.doc2bow(load(f))
+
+class MyDictionary(object):
+	def __iter__(self):
+		f=open("terms.dat","rb")
+		total=load(f)
+		for i in range(total):
+			yield load(f)
 		
+def fetch_dict():
+	print "Fetching Dictionary..."
+	try:
+		dictionary=Dictionary().load("Topic/dic.tm")
+		print "Dictionary loaded!"
+	except IOError:
+		print "Dictionary not found, building Dictionary..."
+		dictionary=Dictionary(i for i in MyDictionary())
+		#once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+		#dictionary.filter_tokens(once_ids)
+		#dictionary.compactify() 
+		print "\rDictionary Built!"
+		print dictionary
+		dictionary.save("Topic/dic.tm")
+	return dictionary
+
+def fetch_model():
+	print "Fetching LDA Model..."
+	try:
+		lda = LdaModel.load('Topic/lda.tm')
+		print "LDA Model loaded!..."
+	except IOError:
+		print "Model not found, building LDA..."
+		corpus=MyCorpus()
+		lda = LdaModel(corpus,num_topics=50,id2word=dictionary,update_every=1,chunksize=1000,passes=15)
+		print "LDA Built!"
+		lda.save('Topic/lda.tm')
+	return lda
+
+def get_suggestions(doc):
+	global dictionary
+	dictionary = fetch_dict()
+	lda        = fetch_model()
+	corpus     = MyCorpus()
+	docs       = list(corpus)
+	return lda[dictionary.doc2bow(doc)]
+
 if __name__ == '__main__':
-	
+
+	print get_suggestions(['added','based','came'])
+
 	"""
-	corpus=MyCorpus()
-	dictionary=Dictionary(i for i in corpus)
-
-	#once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-	#dictionary.filter_tokens(once_ids) # remove stop words and words that appear only once
-	#dictionary.compactify() # remove gaps in id sequence after words that were removed
-
-	dictionary.save("dictionary.dict")
+	dictionary = fetch_dict()
+	lda        = fetch_model()
+	corpus     = MyCorpus()
+	docs       = list(corpus)
+	for i in lda[dictionary.doc2bow(test)]:
+		tmp=""
+		for j in docs[i[0]]:
+			tmp+=dictionary[j[0]]+" "
+		print tmp+"\n"
 	"""
-	dictionary=Dictionary().load("dictionary.dict")
-	
-	corpus=MyCorpus()
-
-	lda = LdaModel(corpus,num_topics=50,id2word=dictionary,update_every=1,chunksize=1000,passes=15)
-	lda.save('lda')
-	pprint(sorted(lda.print_topics(50),reverse=True))
-	
-
