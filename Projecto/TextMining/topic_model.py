92d38827dfa4a6a8c0b6c51e6979b836ddc0bfe9
jsvalenca <jsvalenca@gmail.com>
2014-05-08 12:52:03 +0100

    merge

--- Projecto/TextMining/topic_model.py
+++ Projecto/TextMining/topic_model.py
@@ -8,41 +8,23 @@ from sys import argv
 
 class MyCorpus(object):
 	def __iter__(self):
-		f=open("TopicMod/terms.dat","rb")
+		f=open("Topic/terms.dat","rb")
 		total=load(f)
 		for i in range(total):
 			yield dictionary.doc2bow(load(f))
 
 class MyDictionary(object):
 	def __iter__(self):
-		f=open("TopicMod/terms.dat","rb")
+		f=open("Topic/terms.dat","rb")
 		total=load(f)
 		for i in range(total):
 			yield load(f)
 
-<<<<<<< HEAD
-=======
-class MyNewCorpus(object):
-	def __iter__(self):
-		f=open("TopicMod/new_terms.dat","rb")
-		total=load(f)
-		for i in range(total):
-			yield dictionary.doc2bow(load(f))
-
-class MyNewDictionary(object):
-	def __iter__(self):
-		f=open("TopicMod/new_terms.dat","rb")
-		total=load(f)
-		for i in range(total):
-			yield load(f)
-
-
->>>>>>> 74ebeffa75b0f654abdaf4fc817d657b9f423773
 def get_similar(doc):		
 	def fetch_dict():
 		print "Fetching Dictionary...",
 		try:
-			dictionary=Dictionary().load("TopicMod/dic.tm")
+			dictionary=Dictionary().load("Topic/dic.tm")
 			print "Dictionary loaded!"
 		except IOError:
 			print "Dictionary not found, building Dictionary..."
@@ -52,13 +34,13 @@ def get_similar(doc):
 			dictionary.compactify() 
 			print "\rDictionary Built!"
 			print dictionary
-			dictionary.save("TopicMod/dic.tm")
+			dictionary.save("Topic/dic.tm")
 		return dictionary
 
 	def fetch_model():
 		print "Fetching LDA Model... ",
 		try:
-			lda = LdaModel.load('TopicMod/lda.tm')
+			lda = LdaModel.load('Topic/lda.tm')
 			print "LDA Model loaded!"
 		except IOError:
 			print "Model not found, building LDA..."
@@ -66,13 +48,13 @@ def get_similar(doc):
 			#lda = LdaModel(corpus,num_topics=50,update_every=1,chunksize=1000,passes=15)
 			lda = LdaModel(corpus,num_topics=50,id2word=dictionary,update_every=1,chunksize=1000,passes=50)
 			print "LDA Built!"
-			lda.save('TopicMod/lda.tm')
+			lda.save('Topic/lda.tm')
 		return lda
 
 	def fetch_index(lda):
 		print "Fetching Indexes...   ",
 		try:
-			index = similarities.MatrixSimilarity.load('TopicMod/index.tm')
+			index = similarities.MatrixSimilarity.load('Topic/index.tm')
 			print "Indexes loaded!"
 		except IOError:
 			print "Indexes not found, building Indexes..."
@@ -81,7 +63,7 @@ def get_similar(doc):
 			#pprint(sorted(lda.print_topics(50),reverse=True))
 			index = similarities.MatrixSimilarity(lda[corp])
 			print "Indexes Built!"
-			index.save('TopicMod/index.tm')
+			index.save('Topic/index.tm')
 		
 		return index
 
@@ -97,45 +79,6 @@ def get_similar(doc):
 	
 	return sorted(enumerate(sims),key=lambda item: -item[1])[:10]
 	#return lda[dictionary.doc2bow(doc)]
-
-<<<<<<< HEAD
-=======
-def update_lda():
-	def fetch_dict():
-		print "Fetching Dictionary...",
-		try:
-			dictionary=Dictionary().load("TopicMod/dic.tm")
-			print "Dictionary loaded!"
-			docs=[i for i in MyNewDictionary()]
-			dictionary.add_documents(docs)
-			once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-			dictionary.filter_tokens(once_ids)
-			dictionary.compactify() 
-			print "\rDictionary Built!"
-			print dictionary
-			dictionary.save("TopicMod/new_dic.tm")
-		except IOError:
-			return -1
-		return dictionary
-
-	def fetch_model():
-		print "Fetching LDA Model... ",
-		try:
-			lda = LdaModel.load('TopicMod/lda.tm')
-			print "LDA Model loaded!"
-			corpus=MyNewCorpus()
-			lda.update(corpus,update_every=1,chunksize=50,passes=15)
-			print "LDA Built!"
-			lda.save('TopicMod/new_lda.tm')
-		except IOError:
-			return -1
-		return lda
-
-	global dictionary
-	dictionary = fetch_dict()
-	lda        = fetch_model()
-	print lda.print_topics()
->>>>>>> 74ebeffa75b0f654abdaf4fc817d657b9f423773
 
 if __name__ == '__main__':
 	print get_similar(argv[1])
