541b774af4da969cc89f2facfffff7b898bfc55b
jsvalenca <jsvalenca@gmail.com>
2014-05-08 12:46:35 +0100

    remove updats

--- Projecto/TextMining/topic_model.py
+++ Projecto/TextMining/topic_model.py
@@ -1,12 +1,10 @@
 from gensim.models.ldamodel import LdaModel
 from gensim.models import TfidfModel
-
 from gensim import similarities
-
 from gensim.corpora import Dictionary
-
 from cPickle import load
 from pprint import pprint
+from sys import argv
 
 class MyCorpus(object):
 	def __iter__(self):
@@ -21,21 +19,6 @@ class MyDictionary(object):
 		total=load(f)
 		for i in range(total):
 			yield load(f)
-
-class MyNewCorpus(object):
-	def __iter__(self):
-		f=open("Topic/new_terms.dat","rb")
-		total=load(f)
-		for i in range(total):
-			yield dictionary.doc2bow(load(f))
-
-class MyNewDictionary(object):
-	def __iter__(self):
-		f=open("Topic/new_terms.dat","rb")
-		total=load(f)
-		for i in range(total):
-			yield load(f)
-
 
 def get_similar(doc):		
 	def fetch_dict():
@@ -97,48 +80,9 @@ def get_similar(doc):
 	return sorted(enumerate(sims),key=lambda item: -item[1])[:10]
 	#return lda[dictionary.doc2bow(doc)]
 
-def update_lda():
-	def fetch_dict():
-		print "Fetching Dictionary...",
-		try:
-			dictionary=Dictionary().load("Topic/dic.tm")
-			print "Dictionary loaded!"
-			docs=[i for i in MyNewDictionary()]
-			dictionary.add_documents(docs)
-			once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-			dictionary.filter_tokens(once_ids)
-			dictionary.compactify() 
-			print "\rDictionary Built!"
-			print dictionary
-			dictionary.save("Topic/new_dic.tm")
-		except IOError:
-			return -1
-		return dictionary
-
-	def fetch_model():
-		print "Fetching LDA Model... ",
-		try:
-			lda = LdaModel.load('Topic/lda.tm')
-			print "LDA Model loaded!"
-			corpus=MyNewCorpus()
-			lda.update(corpus,update_every=1,chunksize=50,passes=15)
-			print "LDA Built!"
-			lda.save('Topic/new_lda.tm')
-		except IOError:
-			return -1
-		return lda
-
-	global dictionary
-	dictionary = fetch_dict()
-	lda        = fetch_model()
-	print lda.print_topics()
 
 if __name__ == '__main__':
-
-	test=['list', 'school', 'ofsted', 'sent', 'inspect', 'alleged', 'plot', 'muslim', 'hard-liners', 'seize', 'control', 'governing', 'body', 'published', 'birmingham', 'city', 'council', 'carrying', 'inquiry', 'allegation', 'made', 'public', 'list', '18', 'school', 'ofsted', 'inspected', 'council', 'leader', 'sir', 'albert', 'bore', 'ofsted', 'publish', 'report', 'first', 'second', 'week', 'may', 'council', 'yet', 'seen', 'draft', 'version', 'report', 'sir', 'albert', 'went', 'criticise', 'apparent', 'leaking', 'education', 'funding', 'agency', 'report', 'three', 'city', 'academy', 'national', 'newspaper', 'weekend', 'government', 'cabinet', 'office', 'investigating', 'source', 'trojan', 'horse', 'allegation', 'first', 'came', 'light', 'earlier', 'year', 'contained', 'anonymous', 'unsigned', 'letter', '25', 'school', 'investigated', 'claim', 'male', 'female', 'pupil', 'segregated', 'sex', 'education', 'banned', 'one', 'case', 'al', 'qaida-linked', 'muslim', 'cleric', 'anwar', 'al-awlaki', 'praised', 'assembly', 'preacher', 'killed', 'us', 'drone', 'strike', 'yemen', '2011', 'saturday', 'daily', 'telegraph', 'reported', 'six', 'school', 'implicated', 'allegation', 'faced', 'placed', 'special', 'measure', 'sir', 'albert', 'important', 'sensitive', 'information', 'leaked', 'time', 'wholly', 'reprehensible', 'completely', 'unacceptable', 'one', 'school','word']
-
-	test2=['emma', 'brant', 'newsbeat', 'reporter', 'april', 'last', 'updated', 'hollywood', 'director', 'bryan', 'singer', 'spoken', 'sex', 'abuse', 'accusation', 'made', 'announced', 'miss', 'forthcoming', 'press', 'event', 'new', 'x-men', 'film', 'statement', 'branded', 'claim', 'outrageous', 'vicious', 'false', 'statement', 'came', 'aspiring', 'actor', 'michael', 'egan', 'filed', 'legal', 'case', 'april', 'claiming', 'sexually', 'abused', 'singer', 'accusation', 'date', 'back', 'hawaii', 'california', 'singer', 'statement', 'confirmed', 'withdrawing', 'upcoming', 'medium', 'event', 'latest', 'film', 'want', 'fictitious', 'claim', 'divert', 'attention', 'x-men', 'day', 'future', 'past', 'fantastic', 'film', 'labour', 'love', 'greatest', 'experience', 'career', 'respect', 'extraordinary', 'contribution', 'incredibly', 'talented', 'actor', 'crew', 'involved', 'decided', 'participate', 'upcoming', 'medium', 'event', 'film', 'however', 'promise', 'situation', 'fact', 'show', 'sick', 'twisted', 'shake', 'want', 'thank', 'fan','friend', 'family', 'amazing', 'overwhelming', 'support', 'michael', 'egan', 'claim', 'subjected', 'drug', 'alcohol', 'threat', 'part', 'wider', 'group', 'adult', 'male', 'similarly', 'positioned', 'entertainment', 'industry', 'singer', 'lawyer', 'marty', 'singer', 'director', 'hawaii', 'egan', 'claim', 'abused', 'earlier', 'week', 'singer', 'also', 'accuser', 'lawyer', 'jeff', 'herman', 'behaved', 'reckless', 'outrageous', 'conduct', 'case', 'egan', 'also', 'sued', 'entertainment', 'industry', 'figure', 'former', 'television', 'executive', 'theatre', 'producer', 'claim', 'lured', 'sex', 'ring', 'promise', 'audition', 'acting', 'modelling', 'put', 'payroll', 'actor', 'forced', 'sex', 'men', 'party', 'entertainment', 'industry', 'hollywood', 'us', 'medium', 'decided', 'reveal', 'identity', 'michael', 'egan', 'speaking', 'publicly', 'claim', 'usually', 'name', 'victim', 'sex', 'abuse', 'published', 'america', 'x-men', 'day', 'future', 'past', 'set', 'release', 'may', 'follow', 'bbcnewsbeat', 'twitter']
-	print get_similar(test2)
+	print get_similar(argv[1])
 
 
 	
